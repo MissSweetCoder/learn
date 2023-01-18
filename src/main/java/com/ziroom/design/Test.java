5ae4959f4f2f0fa550c965972873ff389deade81
z <z>
2023-01-18 18:05:28 +0800

    commit

--- src/main/java/com/ziroom/design/Test.java
+++ src/main/java/com/ziroom/design/Test.java
@@ -6,6 +6,7 @@ import com.ziroom.design.单例.IdGenerator2;
 import com.ziroom.design.单例.IdGenerator4;
 import com.ziroom.design.建造者.ResourcePoolConfig;
 
+import java.lang.reflect.Array;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -15,9 +16,7 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
 import java.time.temporal.ChronoField;
 import java.time.temporal.TemporalAdjusters;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.TimeZone;
+import java.util.*;
 import java.util.function.BiConsumer;
 
 /**
@@ -41,19 +40,69 @@ public class Test {
 //        printTimeStamp("2022-11-30 23:59:59");
 //        printTimeStamp("2022-12-01 00:00:00");
 //        printTimeStamp("2022-12-31 23:59:59");
-        System.out.println('e' - 'a');
-        System.out.println('g' - 'd');
+
+//        List<String> list = new ArrayList<>();
+//        list.add("a");
+//        list.add("b");
+//        String[] array = new String[2];
+//        list.toArray(array);
+//        for (int i = 0 ; i < array.length ; i++){
+//            System.out.println(array[i]);
+//        }
+
+//        String[] array = new String[]{"a" , "b"};
+//        List<String> list = Arrays.asList(array);
+//        array[0] = "1";
+//        list.stream().forEach(System.out::println);
+
+//        List<String> a = new ArrayList<String>();
+//        a.add("1");
+//        a.add("2");
+//        for (String temp : a) {
+//            if ("2".equals(temp)) {
+//                a.remove(temp);
+//            }
+//        }
+//        a.stream().forEach(System.out::println);
+
+//        List<String> a = new ArrayList<String>();
+//        a.add("1");
+//        a.add("2");
+//        Iterator<String> iterator = a.iterator();
+//        while (iterator.hasNext()){
+//            System.out.println(iterator.next());
+//            iterator.remove();
+//        }
+
+        Map<String , String> map = new HashMap<>();
+        map.put("key"  , "value");
+        Set<String> keySet = map.keySet();
+        Collection<String> values = map.values();
+        Set<Map.Entry<String, String>> entries = map.entrySet();
+        map.forEach((k , v) -> {
+            System.out.println(k + " " + v);
+        });
+
+        List<String> a = new ArrayList<String>();
+        a.add("1");
+        a.add("2");
+        List<String> list = a.subList(0, 1);
+        list.add(0,"1");
 
 //        SimpleDateFormat sdfutc = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
 //        System.out.println(sdfutc.format(new Date(1669197372755L)));
 
     }
 
-    private static void printTimeStamp(String s) {
+    public static void printTimeStamp(String s) {
         System.out.println(s);
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         LocalDateTime parse = LocalDateTime.parse(s, dateTimeFormatter);
         System.out.println(LocalDateTime.from(parse).atZone(ZoneId.systemDefault()).toInstant().toEpochMilli());
+
+    }
+
+    public static void test(Integer... ids){
 
     }
 
